17bc48f2820c100a8a2c400d72c83767769262f8
wangfujun <hebwfj@163.com>
2013-08-13 15:07:52 +0800

    abc2

---
+++ abc2/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Abc2::Application.config.secret_token = '10614f17a04243732eec3c4024d285d298dd68557b3bf9defd2c746d7d8e56558cbe3ced3a4fcb38bb8d76ef9aa6557a5ad0709f1fd6cb0813622520354eac3e'
---
+++ gitwangfujun/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gitwangfujun::Application.config.secret_token = '351803ead5165dcfa084362ea13e3b5f0e8bf6a0d98789064adeb4209fa6b779d34eecadb0716fa05976bdcc7139380d4fa7a8e0f9bd63b7f9d7d554759daae8'
